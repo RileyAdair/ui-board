02bb319fb427699f62609496cf117866668a5954
Riley Adair <rileydadair@gmail.com>
2017-10-20 15:01:14 -0500

    styled preview

--- public/js/controllers/viewCtrl.js
+++ public/js/controllers/viewCtrl.js
@@ -15,40 +15,64 @@ app.controller('viewCtrl', function($scope, $stateParams, viewSrvc, $rootScope, 
 
   console.log($stateParams);
 
+  $scope.showSidebar = false
+  let toggle = false
+
+  $scope.sidebarToggle = () => {
+    console.log(toggle);
+    if(toggle == false) {
+      console.log('clicked');
+      $('body').find('.right-arrow-icon').css('opacity','0')
+      $('body').find('#item-details-container').css('transform','translate3d(100%,0,0)')
+      toggle = true
+      $scope.showSidebar = true
+    }
+    else {
+      $('body').find('.right-arrow-icon').css('opacity','1')
+      $('body').find('#item-details-container').css('transform','translate3d(0,0,0)')
+      toggle = false
+      $scope.showSidebar = false
+    }
+
+  }
   // Document.ready
   angular.element(document).ready(function(){
 
+
+
+
+
     setTimeout(function(){
-      $('body').find('.view-container').css('opacity', '1')
+      $('body').find('.preview-container').css('opacity', '1')
     }, 10);
 
-    $scope.closePreview = () => {
-      setTimeout(function(){
-        $('body').find('.view-container').css('opacity', '0')
-      }, 10);
-
-      // setTimeout(function(){
-      //   $('body').find('.board-thumbnail-container.selected').css('transition', 'position 2s')
-      //   $('body').find('.board-thumbnail-container.selected').css('position', 'relative')
-      // }, 20);
-
-      setTimeout(function(){
-        $state.go('board');
-      }, 300);
-    }
+    // $scope.closePreview = () => {
+    //   setTimeout(function(){
+    //     $('body').find('.view-container').css('opacity', '0')
+    //   }, 10);
+    //
+    //   // setTimeout(function(){
+    //   //   $('body').find('.board-thumbnail-container.selected').css('transition', 'position 2s')
+    //   //   $('body').find('.board-thumbnail-container.selected').css('position', 'relative')
+    //   // }, 20);
+    //
+    //   setTimeout(function(){
+    //     $state.go('board');
+    //   }, 300);
+    // }
 
 
     // View title
-    $('body').find('.view-title').on('click', function() {
+    $('body').find('.preview-title').on('click', function() {
       $(this).prop('contentEditable', true)
     })
-    $('body').find('.view-title').on('blur', function() {
+    $('body').find('.preview-title').on('blur', function() {
       const title = $(this).text()
       if(title) {
         viewSrvc.updateViewTitle(title, viewId);
       }
     })
-    $('body').find('.view-title').keypress(function(event){
+    $('body').find('.preview-title').keypress(function(event){
       if(event.keyCode == 13){
         $(this).prop('contentEditable', false)
         const title = $(this).text()
@@ -59,16 +83,16 @@ app.controller('viewCtrl', function($scope, $stateParams, viewSrvc, $rootScope, 
 
     });
     // View description
-    $('body').find('.view-description').on('click', function() {
+    $('body').find('.preview-description').on('click', function() {
       $(this).prop('contentEditable', true)
     })
-    $('body').find('.view-description').on('blur', function() {
+    $('body').find('.preview-description').on('blur', function() {
       const description = $(this).text()
       if(description) {
         viewSrvc.updateViewDescription(description, viewId);
       }
     })
-    $('body').find('.view-description').keypress(function(event){
+    $('body').find('.preview-description').keypress(function(event){
       if(event.keyCode == 13){
         $(this).prop('contentEditable', false)
         const description = $(this).text()
@@ -79,16 +103,16 @@ app.controller('viewCtrl', function($scope, $stateParams, viewSrvc, $rootScope, 
 
     });
     // View reference
-    $('body').find('.view-reference-url').on('click', function() {
+    $('body').find('.preview-reference-url').on('click', function() {
       $(this).prop('contentEditable', true)
     })
-    $('body').find('.view-reference-url').on('blur', function() {
+    $('body').find('.preview-reference-url').on('blur', function() {
       const reference = $(this).text()
       if(reference) {
         viewSrvc.updateViewReference(reference, viewId);
       }
     })
-    $('body').find('.view-reference-url').keypress(function(event){
+    $('body').find('.preview-reference-url').keypress(function(event){
       if(event.keyCode == 13){
         $(this).prop('contentEditable', false)
         const reference = $(this).text()
@@ -101,7 +125,11 @@ app.controller('viewCtrl', function($scope, $stateParams, viewSrvc, $rootScope, 
 
   })
 
-
+  // $scope.toggle = true
+  // $scope.sidebarToggle = () => {
+  //   console.log('clicked');
+  //   $scope.toggle = false
+  // }
 
 
   $rootScope.id = viewId;
