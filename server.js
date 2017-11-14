4635a38b6abeaa4a73669562060cab0b8205d9e8
Riley Adair <rileydadair@gmail.com>
2017-11-14 16:17:01 -0600

    test

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const ctrl = require('./server/ctrl.js');
 const { dbUser, database } = require('./server/config');
 // Database connection information
 // const connectionString = `postgres://${dbUser}@localhost/${database}`;
-const connectionString = "postgres://lyqkwtvsnrrqyf:acd804d40b1368b5dbfbf97a607ae16580d26e68bf6a61c5a152b91a7b3cef25@ec2-54-163-245-14.compute-1.amazonaws.com:5432/d1k08g3kh90mfi?ssl=true";
+
 const { port, connectionString } = require('./config')
 // connecting to our DB with massive
 massive(connectionString).then(db => {
